fa59f237cdd2cf5528ad9b776698e2856c6cebc1
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-04-10 20:31:47 +0300

    hibernate project

---
+++ src/main/java/gr/aueb/cf/model/Course.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private Teacher teacher;
+
+    public Course() {
+
+    }
+
+    public Course(Long id, String title, Teacher teacher) {
+        Id = id;
+        this.title = title;
+        this.teacher = teacher;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
